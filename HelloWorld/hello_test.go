29dfa40c961f026cd0c3bfd771554493e52bdc67
wesley <wesleysilvadesousa@gmail.com>
2021-04-08 00:00:51 -0300

    refactor: Add more languages to test and refactor main file

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -12,14 +12,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Wes")
+		got := Hello("Wes", "")
 		want := "Hello, Wes"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Wes", "Spanish")
+		want := "Hola, Wes"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T){
+		got := Hello("Wes", "French")
+		want := "Bonjour, Wes"
 		assertCorrectMessage(t, got, want)
 	})
 
