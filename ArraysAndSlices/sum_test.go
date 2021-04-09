def5385c9b1c609f2818c205bd8497e79d213222
wesley <wesleysilvadesousa@gmail.com>
2021-04-09 17:22:30 -0300

    refactor: Tests for any array size

--- ArraysAndSlices/sum_test.go
+++ ArraysAndSlices/sum_test.go
@@ -3,14 +3,26 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [3]int{1, 2, 3}
-	got := Sum(numbers)
-	want := 6
 
-	if got != want {
-        t.Errorf("got %d want %d given, %v", got, want, numbers)
-    }
+	t.Run("Collection of 5 numbers", func (t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
 
-	
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	// t.Run("Collection of any size", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3}
+	// 	got := Sum(numbers)
+	// 	want := 6
+
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+
+	// })
+
 }
-
