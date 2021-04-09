e81451bd7584f4b7ab7e54dc8dd9e26310ebfbe7
wesley <wesleysilvadesousa@gmail.com>
2021-04-09 16:57:11 -0300

    test: Create sum test from a array

---
+++ ArraysAndSlices/sum_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [3]int{1, 2, 3}
+	got := Sum(numbers)
+	want := 6
+
+	if got != want {
+        t.Errorf("got %d want %d given, %v", got, want, numbers)
+    }
+
+	
+}
+
